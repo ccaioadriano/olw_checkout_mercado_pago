377d8f70923c26cfc6723ae866119b305a7959a6
Caio Adriano <caiobhadriano@gmail.com>
2025-05-15 16:39:04 -0300

    Adicionando modelos e enums com relacionamentos e propriedades preenchíveis

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Address extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+    
+    protected $fillable = [
+        'user_id',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'district',
+        'number',
+        'complement',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
